9b5d88b222c6a5e175e7256280016aa0a8e3026e
mhuan2011 <minhhuan1100@gmail.com>
2021-03-30 20:40:20 +0700

    first commit

---
+++ src/dangky/ThietBi.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dangky;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Minh Huan
+ */
+public class ThietBi {
+    String matb, tenTB, maLoai, moTa, tinhTrang;
+    boolean check = false;
+}
